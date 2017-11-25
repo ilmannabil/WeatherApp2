62f307884679f9d79a533b7cbabb71afb516f50f
ilmannabil <nabilnurilman@gmail.com>
2017-11-25 14:09:44 +0700

    Update Weather Fragment

--- app/src/main/java/com/example/ilman/weatherapp2/ui/fragment/WeatherFragment.java
+++ app/src/main/java/com/example/ilman/weatherapp2/ui/fragment/WeatherFragment.java
@@ -1,7 +1,11 @@
 package com.example.ilman.weatherapp2.ui.fragment;
 
 
+import android.graphics.drawable.Drawable;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,7 +21,12 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.ilman.weatherapp2.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -54,10 +63,67 @@ public class WeatherFragment extends Fragment {
         //TODO code
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onResponse(JSONObject response) {
-                Log.d("SUKSES RESPONSE",String.valueOf(response));
+//                Log.d("SUKSES RESPONSE",String.valueOf(response));
 
+                try {
+                    JSONObject cityObject = response.getJSONObject("city");
+                    String country = cityObject.getString("country");
+                    String city = cityObject.getString("name");
+
+                    JSONArray listArray= response.getJSONArray("list");
+                    JSONObject firstObject= listArray.getJSONObject(0);
+
+                    JSONObject mainObject= firstObject.getJSONObject("main");
+                    Double tempMax= mainObject.getDouble("temp_max");
+                    Double tempMin= mainObject.getDouble("temp_min");
+
+                    JSONArray weatherArray= firstObject.getJSONArray("weather");
+                    JSONObject weatherFirstObject= weatherArray.getJSONObject(0);
+                    String status= weatherFirstObject.getString("main");
+
+                    String date= firstObject.getString("dt_txt");
+
+
+                    tvCity.setText(city+", "+country);
+                    tvMaxTemp.setText(String.valueOf(tempMax));
+                    tvMinTemp.setText(String.valueOf(tempMin));
+                    tvStatus.setText(status);
+
+                    if (status.equals("Rain")){
+                        imgCuaca.setImageResource(R.drawable.rain_large);
+                    }else if (status.equals("Clouds")){
+                        imgCuaca.setImageResource(R.drawable.coldly_large);
+                    }else{
+                        imgCuaca.setImageResource(R.drawable.sun_large);
+                    }
+
+
+                    SimpleDateFormat formatDefault = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    SimpleDateFormat formatTimeCustom = new SimpleDateFormat("hh.mm");
+                    SimpleDateFormat formatDateCustom = new SimpleDateFormat("EEEE, dd MMM");
+
+                    String time=date;
+                    String datecustom=date;
+                    try {
+                        Date timesFormat = formatDefault.parse(time);
+                        time=formatTimeCustom.format(timesFormat);
+
+                        Date dateFormat= formatDefault.parse(date);
+                        datecustom=formatDateCustom.format(dateFormat);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    tvTime.setText(time);
+                    tvDayDate.setText(datecustom);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
 
             }
